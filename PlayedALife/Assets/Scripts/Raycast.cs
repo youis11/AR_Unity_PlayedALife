8e395d943ea2c734d5ccc813966830b172a59579
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2020-05-08 22:21:18 +0200

    Raycast working!!! :battery:  Let's try vuforia raycast :D

--- PlayedALife/Assets/Scripts/Raycast.cs
+++ PlayedALife/Assets/Scripts/Raycast.cs
@@ -16,7 +16,8 @@ public class Raycast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width*0.5f, Screen.height*0.5f, 0));
+        //Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
