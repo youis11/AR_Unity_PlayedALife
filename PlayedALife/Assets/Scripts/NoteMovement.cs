7d3502958aa524205a83054f1489cb49298d1aa8
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2020-05-09 21:34:40 +0200

    Movement added

---
+++ PlayedALife/Assets/Scripts/NoteMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteMovement : MonoBehaviour
+{
+    bool start = false;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            start = true;
+        
+        if(start)
+            transform.Translate(Vector3.forward * -speed * Time.deltaTime);
+
+    }
+}
--- PlayedALife/Assets/Scripts/Raycast.cs
+++ PlayedALife/Assets/Scripts/Raycast.cs
@@ -6,28 +6,83 @@ public class Raycast : MonoBehaviour
 {
     public Camera camera;
     [SerializeField] Material highlightMaterial;
+    [SerializeField] Material defaultMaterial;
 
-    // Start is called before the first frame update
+    Transform _selection;
+
+    [SerializeField] string selectableGreenTag;
+    [SerializeField] string selectableRedTag;
+    [SerializeField] string selectableBlueTag;
+
+    string color_selected;
+    string color_green = "Green";
+    string color_blue = "Blue";
+    string color_red = "Red";
+    string color_empty = "Empty";
+
+
+    int layerMask = 1 << 8; //Number 8: Selectable
+
+    float time_pressed = 0;
+
     void Start()
     {
     }
 
-
-    // Update is called once per frame
     void Update()
     {
-        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width*0.5f, Screen.height*0.5f, 0));
-        //Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        
+        if(_selection != null)
         {
-            var selection = hit.transform;
-            var selectionRenderer = selection.GetComponent<Renderer>();
-            if(selectionRenderer != null)
+            Renderer selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width*0.5f, Screen.height*0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+
+            Transform selection = hit.transform;
+            if((selection.CompareTag(selectableGreenTag) || selection.CompareTag(selectableBlueTag) || selection.CompareTag(selectableRedTag))/* && time_pressed <= 0.5f*/)
             {
-                selectionRenderer.material = highlightMaterial;
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                }
+                _selection = selection;
+
+                if (selection.CompareTag(selectableGreenTag))
+                    color_selected = color_green;
+                if (selection.CompareTag(selectableBlueTag))
+                    color_selected = color_blue;
+                if (selection.CompareTag(selectableRedTag))
+                    color_selected = color_red;
+
+                //time_pressed += Time.deltaTime;
             }
+            else
+            {
+                color_selected = color_empty;
+            }
+        }
+        else
+        {
+            color_selected = color_empty;
+            //time_pressed = 0;
         }
 
     }
+
+    public string ReturnSelected()
+    {
+        if (color_selected != color_empty)
+            return color_selected;
+        else
+            return color_empty;   
+    }
+
 }
