c882a66e5da620c93422a3898c613b7d88e111ec
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2020-05-18 18:44:17 +0200

    Scene improved

--- Rose/Assets/Scripts/ReleaseNote.cs
+++ Rose/Assets/Scripts/ReleaseNote.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class ReleaseNote : MonoBehaviour
 {
-    [SerializeField] string player;
+    [SerializeField] string playerTag;
     [SerializeField] string noteTag;
-    public GameObject selectionManager;
-    // Start is called before the first frame update
+    //public GameObject selectionManager;
+
+    [SerializeField] Material highlightMaterial_ReleaseBlue;
+    [SerializeField] Material highlightMaterial_ReleaseOrange;
+    [SerializeField] Material highlightMaterial_ReleasePurple;
+    [SerializeField] Material defaultMaterial_BaseBlue;
+    [SerializeField] Material defaultMaterial_BaseOrange;
+    [SerializeField] Material defaultMaterial_BasePurple;
+
+    Renderer releaserRenderer;
     void Start()
     {
+        releaserRenderer = GetComponent<Renderer>();
+
     }
 
     // Update is called once per frame
@@ -20,16 +30,56 @@ public class ReleaseNote : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == player && other.tag == noteTag)
+        if (other.tag == playerTag && other.tag == noteTag)
         {
             // TODO: White particles going up with trail and score++
         }
-        if (other.tag == player)
+        if (other.tag == playerTag)
         {
-            // TODO change material 
+            ActiveReleasers();
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == playerTag)
+        {
+            RestartReleasers();
         }
         
     }
 
-    
+    void RestartReleasers()
+    {
+        if (transform.tag == "Orange_Releaser")
+        {
+            releaserRenderer.material = defaultMaterial_BaseOrange;
+        }
+        if (transform.tag == "Purple_Releaser")
+        {
+            releaserRenderer.material = defaultMaterial_BasePurple;
+        }
+        if (transform.tag == "Blue_Releaser")
+        {
+            releaserRenderer.material = defaultMaterial_BaseBlue;
+        }
+
+    }
+    void ActiveReleasers()
+    {
+        if (transform.tag == "Orange_Releaser")
+        {
+            releaserRenderer.material = highlightMaterial_ReleaseOrange;
+        }
+        if (transform.tag == "Purple_Releaser")
+        {
+            releaserRenderer.material = highlightMaterial_ReleasePurple;
+        }
+        if (transform.tag == "Blue_Releaser")
+        {
+            releaserRenderer.material = highlightMaterial_ReleaseBlue;
+        }
+
+    }
+
 }
