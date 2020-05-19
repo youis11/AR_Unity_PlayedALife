4f89e762a6f527a08d6d1e1313c214ad6c2477da
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2020-05-19 17:35:58 +0200

    Im afraid... I sin.... Now UI moves target player

--- Rose/Assets/Scripts/Player.cs
+++ Rose/Assets/Scripts/Player.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject BlueBase;
+    public GameObject PurpleBase;
+    public GameObject OrangeBase;
+
+    Vector3 nextTargetPosition;
+    string actualTarget;
+
     void Start()
+    {
+        actualTarget = PurpleBase.transform.tag;
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickLeft()
     {
+        if(actualTarget == BlueBase.transform.tag)
+        {
+            nextTargetPosition = BlueBase.transform.position;
+            actualTarget = BlueBase.transform.tag;
+        }
+        else if (actualTarget == PurpleBase.transform.tag)
+        {
+            nextTargetPosition = BlueBase.transform.position;
+            actualTarget = BlueBase.transform.tag;
+        }
+        else if (actualTarget == OrangeBase.transform.tag)
+        {
+            nextTargetPosition = PurpleBase.transform.position;
+            actualTarget = PurpleBase.transform.tag;
+        }
+
+        transform.position = nextTargetPosition;
         
+    }
+
+    public void OnClickRight()
+    {
+        if (actualTarget == BlueBase.transform.tag)
+        {
+            nextTargetPosition = PurpleBase.transform.position;
+            actualTarget = PurpleBase.transform.tag;
+        }
+        else if (actualTarget == PurpleBase.transform.tag)
+        {
+            nextTargetPosition = OrangeBase.transform.position;
+            actualTarget = OrangeBase.transform.tag;
+        }
+        else if (actualTarget == OrangeBase.transform.tag)
+        {
+            nextTargetPosition = OrangeBase.transform.position;
+            actualTarget = OrangeBase.transform.tag;
+        }
+
+        transform.position = nextTargetPosition;
     }
 }
