292c160f3ed1c8b55dc2638b1ae9eff4bfbc68ea
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2020-05-19 18:11:26 +0200

    Ta tudo bem tudo bem

---
+++ Rose/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public GameObject BlueNote;
+    public GameObject PurpleNote;
+    public GameObject OrangeNote;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == BlueNote.tag)
+        {
+            Destroy(other.gameObject);
+
+        }
+        if (other.tag == PurpleNote.tag)
+        {
+            Destroy(other.gameObject);
+        }
+        if (other.tag == OrangeNote.tag)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
